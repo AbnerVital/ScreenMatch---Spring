9935e33519239d8731759acacdbe3c8e87d68027
Abner Vital <avgchaves@gmail.com>
2025-06-03 17:03:49 -0300

    feat: integrar API MyMemory para tradução automática de sinopses
    - Adiciona classe ConsultaMyMemory com método obterTraducao
    - Utiliza ConsumoAPI para realizar requisição GET
    - Cria modelos DadosTraducao e DadosResposta para mapear JSON retornado
    - Aplica tradução à sinopse no construtor de Serie

--- src/main/java/br/com/alura/Screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/Screenmatch/model/Serie.java
@@ -1,5 +1,7 @@
 package br.com.alura.Screenmatch.model;
 
+import br.com.alura.Screenmatch.service.traducao.ConsultaMyMemory;
+
 import java.util.OptionalDouble;
 
 public class Serie {
@@ -24,7 +26,7 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = dadosSerie.sinopse();
+        this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse().trim());
     }
 
     public String getTitulo() {
