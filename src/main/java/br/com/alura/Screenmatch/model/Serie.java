4731b11224aacbd64b80398a7ef9f25b2d798d82
Abner Vital <avgchaves@gmail.com>
2025-06-05 11:53:36 -0300

    feat: Integra persistência de séries com Spring Data JPA
    - Cria entidade Serie com anotação @Entity e mapeamentos JPA
    - Implementa SerieRepository estendendo JpaRepository
    - Injeta e utiliza repositório no fluxo principal para salvar dados

--- src/main/java/br/com/alura/Screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/Screenmatch/model/Serie.java
@@ -1,15 +1,26 @@
 package br.com.alura.Screenmatch.model;
 
 import br.com.alura.Screenmatch.service.traducao.ConsultaMyMemory;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
+@Entity
+@Table (name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
 
     private Integer totalTemporadas;
 
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
 
     private Categoria genero;
 
@@ -19,6 +30,9 @@ public class Serie {
 
     private String sinopse;
 
+    @Transient
+    private List<DadosEpisodio> episodio = new ArrayList<>();
+
     public Serie (DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
         this.totalTemporadas = dadosSerie.totalTemporadas();
@@ -27,6 +41,18 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
         this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse().trim());
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<DadosEpisodio> getEpisodio() {
+        return episodio;
+    }
+
+    public void setEpisodio(List<DadosEpisodio> episodio) {
+        this.episodio = episodio;
     }
 
     public String getTitulo() {
@@ -83,6 +109,10 @@ public class Serie {
 
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     @Override
