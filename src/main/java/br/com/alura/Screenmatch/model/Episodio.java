242b0663015eee7d754e9dc3000c4f50c0a149b9
Abner Vital <avgchaves@gmail.com>
2025-06-08 09:41:26 -0300

    feat(domain): cria entidade Episodio com mapeamento para Serie

--- src/main/java/br/com/alura/Screenmatch/model/Episodio.java
+++ src/main/java/br/com/alura/Screenmatch/model/Episodio.java
@@ -1,14 +1,32 @@
 package br.com.alura.Screenmatch.model;
 
+import jakarta.persistence.*;
+
 import java.time.DateTimeException;
 import java.time.LocalDate;
 
+@Entity
+@Table (name = "episodios")
 public class Episodio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private Integer temporada;
+
     private String titulo;
+
     private Integer numeroEpsodio;
+
     private Double avaliacao;
+
     private LocalDate dataLancamento;
+
+    @ManyToOne
+    private Serie serie;
+
+    private Episodio(){}
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
@@ -26,6 +44,41 @@ public class Episodio {
 
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setNumeroEpsodio(Integer numeroEpsodio) {
+        this.numeroEpsodio = numeroEpsodio;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
 
     public Integer getTemporada() {
         return temporada;
