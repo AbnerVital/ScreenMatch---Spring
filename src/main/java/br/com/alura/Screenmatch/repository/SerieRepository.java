234a1c005a43edc9da8048d04e59095cfebaceb2
Abner Vital <avgchaves@gmail.com>
2025-06-09 14:01:45 -0300

    feat(repository): adiciona método findByTituloContainingIgnoreCase

--- src/main/java/br/com/alura/Screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/Screenmatch/repository/SerieRepository.java
@@ -3,6 +3,9 @@ package br.com.alura.Screenmatch.repository;
 import br.com.alura.Screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
 
+    Optional<Serie>findByTituloContainingIgnoreCase(String nomeSerie);
 }
