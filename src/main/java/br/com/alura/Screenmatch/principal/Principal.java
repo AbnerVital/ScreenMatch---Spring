4cc56326af0b2e3050e78a7388cf26359ff62d94
Abner Vital <avgchaves@gmail.com>
2025-06-09 17:46:14 -0300

    feat: adiciona busca de séries por ator com filtro de avaliação mínima

--- src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -34,6 +34,7 @@ public class Principal {
                     2 - Buscar episódios
                     3 - Buscar séries listadas
                     4 - Buscar série por nome
+                    5 - Buscar séries por ator
                     0 - Sair                                 
                     """;
 
@@ -53,6 +54,9 @@ public class Principal {
                     break;
                 case 4:
                     buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -135,5 +139,16 @@ public class Principal {
         } else {
             System.out.println("série não encontrada!");
         }
+    }
+
+    private void buscarSeriesPorAtor() {
+        buscarSeriesListadas();
+        System.out.println("Digite o nome do ator para a busca: ");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliação a partir de qual valor? ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
+        seriesEncontradas.forEach(s -> System.out.println(s.getTitulo() + " - Avaliação: " + s.getAvaliacao()));
     }
 }
