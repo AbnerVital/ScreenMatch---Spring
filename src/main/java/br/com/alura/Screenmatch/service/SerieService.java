97f27b2159e58b182f0e23a7750b547911cff3c0
Abner Vital <avgchaves@gmail.com>
2025-06-19 13:57:18 -0300

    feat: implementa método obterTop5Series() para listagem das séries com maiores avaliações

--- src/main/java/br/com/alura/Screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/Screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.Screenmatch.service;
 
 import br.com.alura.Screenmatch.dto.SerieDTO;
+import br.com.alura.Screenmatch.model.Serie;
 import br.com.alura.Screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,15 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries(){
-        return repositorio.findAll().
-                stream()
+        return conversorDTO(repositorio.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series(){
+        return conversorDTO(repositorio.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> conversorDTO (List<Serie> series){
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
     }
