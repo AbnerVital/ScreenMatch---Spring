ac50315d71480b0c309df4047dfc1bb432caa71a
Abner Vital <avgchaves@gmail.com>
2025-06-20 05:48:50 -0300

    feat: adiciona método para buscar série por ID e converter para DTO

--- src/main/java/br/com/alura/Screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/Screenmatch/service/SerieService.java
@@ -6,7 +6,9 @@ import br.com.alura.Screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -14,21 +16,31 @@ public class SerieService {
     @Autowired
     private SerieRepository repositorio;
 
-    public List<SerieDTO> obterTodasAsSeries(){
+    public List<SerieDTO> obterTodasAsSeries() {
         return conversorDTO(repositorio.findAll());
     }
 
-    public List<SerieDTO> obterTop5Series(){
+    public List<SerieDTO> obterTop5Series() {
         return conversorDTO(repositorio.findTop5ByOrderByAvaliacaoDesc());
     }
 
-    public List<SerieDTO> obterTopLancamentos(){
-        return conversorDTO(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    public List<SerieDTO> obterTopLancamentos() {
+        return conversorDTO(repositorio.encontrarEpisodiosMaisRecentes());
     }
 
-    private List<SerieDTO> conversorDTO (List<Serie> series){
+    private List<SerieDTO> conversorDTO(List<Serie> series) {
         return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }else {
+            return null;
+        }
     }
 }
